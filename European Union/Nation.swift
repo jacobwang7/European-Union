40c68b6d7cbafb5cfc3eb934e798be47eef6956e
Jacob P Wang <59838504+jacobwang7@users.noreply.github.com>
2021-02-28 14:43:33 -0500

    More updates!

---
+++ European Union/Nation.swift
@@ -0,0 +1,14 @@
+//
+//  Nation.swift
+//  European Union
+//
+//  Created by Jacob Wang on 2/28/21.
+//
+
+import Foundation
+
+struct Nation {
+    var country: String
+    var capital: String
+    var usesEuro: Bool
+}
